172ded07a25e337828dda80ba04de6200cc49bdc
Webbouwer <support@webdesigndenhaag.net>
2020-12-17 08:35:38 +0100

    Create littletweak.php

---
+++ littletweak.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: littletweak
+Plugin URI: https://webbouwer.org
+Description: fix common wordpress irritations
+Version:     0.5.2
+Author:      Webbouwer
+Author URI:  http://webbouwer.org
+Text Domain: littletweak
+License:     © Webbouwer All rights reserved
+License URI: https://webbouwer.org
+Github Plugin URI: https://github.com/webbouwer/littletweak
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
